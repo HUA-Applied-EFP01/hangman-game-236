0a25dd7452358e42be83080f6e15d9088deaf931
Maria Bakali <mbakali@ots.local>
2024-02-05 00:09:14 +0200

    minor changer and read me

--- hangman.py
+++ hangman.py
@@ -1,5 +1,4 @@
 import time
-
 class HangmanGame:
     def __init__(self, word):
         self.word = word.lower()  # ορισμός της λέξης και αρχικοποίηση μεταβλητών, μετατροπή της λέξης σε πεζά γράμματα
@@ -7,7 +6,7 @@ class HangmanGame:
         self.guessed_letters = set()  # σύνολο που κρατάει τα γράμματα που έχει μαντέψει ο χρήστης χωρίς να μετράει τα γράμματα που μπορεί να επαναληφθούν
         self.word_guesses_left = 2  # ορίζουμε πόσες φορές μπορεί να μαντέψει ολόκληρη την λέξη ο παίκτης
         self.words_guessed = set()  # σύνολο που μετράει πόσες φορές έχει μαντέψει ολόκληρη την λέξη ο παίκτης
-
+    
     def display_word(self):
         display = ''
         if self.word not in self.words_guessed:
@@ -19,10 +18,9 @@ class HangmanGame:
         else:
             display = self.word
         return display
-
+   
     def make_guess(self, guess):  # ο παίκτης μαντεύει ένα γράμμα κάθε φορά
         guess = guess.lower()
-
         if guess.isalpha() and len(guess) == 1:
             if guess in self.guessed_letters:
                 print("You already guessed that letter.")  # έλεγχος εάν ο χρήστης έχει μαντέψει ήδη το γράμμα
@@ -31,14 +29,12 @@ class HangmanGame:
             else:
                 print("Wrong guess!")
                 self.guesses_left -= 1  # μείωση των εναπομεινάντων προσπαθειών
-
             self.guessed_letters.add(guess)
         else:
             print("Invalid input. Please enter a single letter.")  # Εμφάνιση μηνύματος λάθους για μη έγκυρη εισαγωγή
-
+    
     def make_word_guess(self, guess):  # ο παίκτης μαντεύει ολόκληρη τη λέξη
         guess = guess.lower()
-
         if guess in self.words_guessed:
             print("You already guessed that word.")  # έλεγχος εάν ο χρήστης έχει μαντέψει ήδη τη λέξη
         elif guess == self.word:
@@ -47,7 +43,7 @@ class HangmanGame:
             print("Wrong guess!")
             self.word_guesses_left -= 1  # μείωση των εναπομεινάντων προσπαθειών
         self.words_guessed.add(guess)
-
+   
     def check_game_status(self):
         if '_' not in self.display_word():
             print("Congratulations! You guessed the word:", self.word)
@@ -62,8 +58,6 @@ class HangmanGame:
             time.sleep(.6)
             print("Whole word guesses left:", self.word_guesses_left)
             return False  # επιστροφή False αν το παιχνίδι δεν έχει ολοκληρωθεί
-
-
 
 def play_hangman():
     time.sleep(.6)
